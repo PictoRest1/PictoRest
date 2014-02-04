31565371500f1dc9e28327c3807ff8a60b986a87
Johan Kriouche <johan.kriouche@gmail.com>
2014-02-04 10:14:40 +0100

    Creation html/CSS

---
+++ anim.js
@@ -0,0 +1,15 @@
+$( document ).ready(function() {
+
+	jQuery("#connexion").click(function	(){
+		jQuery(".connexion").show("fast");
+	});
+	jQuery("#close_co").click(function (){
+		jQuery(".connexion").hide("fast");	
+	});
+	jQuery("#inscription").click(function	(){
+		jQuery(".inscription").show("fast");
+	});
+	jQuery("#close_ins").click(function (){
+		jQuery(".inscription").hide("fast");	
+	});
+});
