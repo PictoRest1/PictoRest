c73b0ee669d822ced979df4e24f29815c3b5cd4f
johanjudai <p.escalle@live.fr>
2014-02-06 14:57:01 +0100

    ajout des fonctions d'ajout et delete

--- php/Controleur/FrontControleur.php
+++ php/Controleur/FrontControleur.php
@@ -33,19 +33,114 @@ class FrontControleur{
                         
                         
                  });
+                 
+                $app->get( '/user/:id', function($id) use ($app) {
+                    try {
+                        $user = Utilisateur::find($id)->toJson();
+                        echo $user;
+                    } catch(PDOException $e) {
+                        echo '{"error":{"text":'. $e->getMessage() .'}}';
+                    }
+                });
+                
+                $app->get( '/ajoutalbum', function() use ($app) {
+                    try {
+                        $album = new Album();
+                        $id = $_SESSION['idUtil'];
+                        $libelle = $app->request->post('libelle');
+                        $album->libelle=$libelle;$album->idUtil=$id;$album->date=date(Y-m-d);
+                        $album->save();
+                        echo "Album créé !";
+                    } catch(PDOException $e) {
+                        echo '{"error":{"text":'. $e->getMessage() .'}}';
+                    }
+                });
+                
+                $app->get( '/ajoutphoto', function() use ($app) {
+                    try {
+                        $photo = new Photo();
+                        $libelle = $app->request->post('libelle');
+                        $description = $app->request->post('description');
+                        $id = $app->request->post('idAlbum');
+                        $photo->libelle=$libelle;$photo->description=$description;$photo->date=date(Y-m-d);$photo->idAlbum=$id;
+                        $photo->save();
+                        echo "Photo ajoutée !";
+                    } catch(PDOException $e) {
+                        echo '{"error":{"text":'. $e->getMessage() .'}}';
+                    }
+                });
+                
+                $app->get( '/ajoutuser', function() use ($app) {
+                    try {
+                        $user = new Utilisateur();
+                        $pseudo = $app->request->post('pseudo');
+                        $mail = $app->request->post('mail');
+                        $mdp = sha1($app->request->post('mdp'));
+                        $user->pseudo=$pseudo;$user->mail=$mail;$user->mdp=$mdp;
+                        $user->save();
+                        echo "Utilisateur ajouté !";
+                    } catch(PDOException $e) {
+                        echo '{"error":{"text":'. $e->getMessage() .'}}';
+                    }
+                });
+                
+                $app->get( '/deletephoto', function() use ($app) {
+                    try {
+                        $id = $app->request->post('idPhoto');
+                        $photo = Photo::find($id);
+                        $photo->delete();
+                        echo "Photo supprimée !";
+                    } catch(PDOException $e) {
+                        echo '{"error":{"text":'. $e->getMessage() .'}}';
+                    } catch (ModelNotFoundException $e) {
+                        echo '{"error":{"text":'. $e->getMessage() .'}}';
+                    }
+                });
+                
+                $app->get( '/deletealbum', function() use ($app) {
+                    try {
+                        $id = $app->request->post('idAlbum');
+                        $album = Album::find($id);
+                        $photos = Photo::findAll()->where('idAlbum', '=', $id);
+                        foreach ($photos as $photo) {
+                            $photo->delete();
+                        }
+                        $album->delete();
+                        echo "Album supprimé !";
+                    } catch(PDOException $e) {
+                        echo '{"error":{"text":'. $e->getMessage() .'}}';
+                    } catch (ModelNotFoundException $e) {
+                        echo '{"error":{"text":'. $e->getMessage() .'}}';
+                    }
+                });
+                
+                $app->get( '/deleteuser', function() use ($app) {
+                    try {
+                        $id = $app->request->post('idUtil');
+                        $user = Utilisateur::find($id);
+                        $albums = Album::findAll()->where('idUtil', '=', $id);
+                        foreach ($albums as $album) {
+                            $idAl = $album->id;
+                            $photos = Photo::findAll()->where('idAlbum', '=', $idAl);
+                            foreach ($photos as $photo) {
+                                $photo->delete();
+                            }
+                            $album->delete();
+                        }
+                        $user->delete();
+                        echo "Utilisateur supprimé !";
+                    } catch(PDOException $e) {
+                        echo '{"error":{"text":'. $e->getMessage() .'}}';
+                    } catch (ModelNotFoundException $e) {
+                        echo '{"error":{"text":'. $e->getMessage() .'}}';
+                    }
+                });
+                
                 $app->group("/rest", function() use ($app) {
                     $app->get('/users/auth', function() use ($app) {
                         try {
                             $identifiant = $app->request->get('userid');
                             $user = Utilisateur::where('pseudo', '=', $identifiant)->toJson();
-                            echo $user;
-                        } catch(PDOException $e) {
-                            echo '{"error":{"text":'. $e->getMessage() .'}}';
-                        }
-                    });
-                    $app->get( '/users/:id', function($id) use ($app) {
-                        try {
-                            $user = Utilisateur::find($id)->toJson();
                             echo $user;
                         } catch(PDOException $e) {
                             echo '{"error":{"text":'. $e->getMessage() .'}}';
